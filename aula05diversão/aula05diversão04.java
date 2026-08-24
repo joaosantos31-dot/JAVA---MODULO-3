6b14b9a6802cc4081e3cb9b172775865b61a7f55
joaosantos31-dot <joao.santos31@ba.estudante.senai.br>
2026-08-24 16:00:26 -0300

    Add files via upload

--- aula05diversão/aula05diversão04.java
+++ aula05diversão/aula05diversão04.java
@@ -2,8 +2,10 @@ package aula05diversão;
 
 public class aula05diversão04 {
     public static void main(String[] args) {
-        for(int x=1;x<=10;x++)
+        for(int x=1;x<=10;x++){
+            System.out.println("Tabuada do "+x);
             for(int i=1;i<=10;i++)
                 System.out.println(x+" X "+i+" = "+(x*i));
+        }
     }
 }
